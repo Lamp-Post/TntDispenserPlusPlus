b8a8239e52754d7610801c562d2346dac3e3262d
Kees Huiberts <itissohardtothinkofagoodemail@gmail.com>
2012-01-21 14:08:25 +0100

    added licence

--- src/main/java/org/beide/tntdispenser/tntDispenser.java
+++ src/main/java/org/beide/tntdispenser/tntDispenser.java
@@ -9,6 +9,23 @@ import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.event.Event;
 
+/*
+ * This file is part of tntDispenser.
+ * 
+ * tntDispenser is free software: you can redistribute it and/or modify *
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * tntDispenser is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with tntDispenser.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 public class tntDispenser extends JavaPlugin {
 	private static Logger log = Logger.getLogger("Minecraft");
 	PluginDescriptionFile desc;
